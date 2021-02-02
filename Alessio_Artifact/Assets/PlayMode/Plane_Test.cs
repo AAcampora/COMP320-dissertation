c2cb5c4de2f4d37002cf491d655fa2ccf95e4a95
Alessio Acampora <aa202405@falmouth.ac.uk>
2021-02-02 18:22:02 +0000

    added unit test bedframe and implemented plane class

---
+++ Alessio_Artifact/Assets/PlayMode/Plane_Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Plane_Test
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator North()
+        {
+            // Use the Assert class to test conditions.
+
+            var gameObject = new GameObject();
+            var plane = gameObject.AddComponent<Plane>();
+            
+            // Use yield to skip a frame.
+            yield return new WaitForSeconds(2);
+
+            Assert.AreEqual(10.0f, plane.North);
+        }
+    }
+}
---
+++ Alessio_Artifact/Assets/Scripts/Plane.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+    public float North = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        North = 10.0f;
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            print("forward");
+        }
+    }
+}
