9e0a6c5b5c6159359bec3696cd270eb3d23f78d2
Alessio Acampora <aa202405@falmouth.ac.uk>
2021-02-24 08:42:24 +0000

    added Plane movement and Level

--- Alessio_Artifact/Assets/PlayMode/Plane_Test.cs
+++ Alessio_Artifact/Assets/PlayMode/Plane_Test.cs
@@ -11,16 +11,20 @@ namespace Tests
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator North()
+        //sut_when_then
+        public IEnumerator North_Check_Active()
         {
             // Use the Assert class to test conditions.
 
+            //arrange
             var gameObject = new GameObject();
             var plane = gameObject.AddComponent<Plane>();
-            
-            // Use yield to skip a frame.
-            yield return new WaitForSeconds(2);
 
+            // Use yield to skip a frame.
+            //act
+            yield return new WaitForFixedUpdate();
+
+            //assert
             Assert.AreEqual(10.0f, plane.North);
         }
     }
