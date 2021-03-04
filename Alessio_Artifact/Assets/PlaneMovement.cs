46611d12d4c8d2d8da3cdbbe09b1806ccd311ce3
Alessio Acampora <aa202405@falmouth.ac.uk>
2021-03-04 04:21:00 +0000

    added helper functions

--- Alessio_Artifact/Assets/PlaneMovement.cs
+++ Alessio_Artifact/Assets/PlaneMovement.cs
@@ -15,13 +15,21 @@ public class PlaneMovement : MonoBehaviour
     public float rotSpeedX = 3.0f;
     public float rotSpeedY = 1.5f;
 
+    Vector3 pitch;
+
+    Vector3 moveVector;
+
     private Vector3 gravity = Physics.gravity;
 
     Vector3 propelPlane;
 
     public bool isGrounded;
 
+    public bool isAboutToCollide;
+
     public bool isLandingGear = true;
+
+    public bool isHelperOn;
     
     public enum EPlaneState { FLYING, DEPARTING, LANDING };
 
@@ -98,18 +106,28 @@ public class PlaneMovement : MonoBehaviour
                 landingGear.SetActive(false);
                 isLandingGear = true;
             }
+
+        StallingHandler();
+        GroundHelper();
+        TurnHelper();
     }
 
     private Vector3 PropelPlane(float speed)
     {
 
-        Vector3 moveVector = transform.forward * speed;
+        moveVector = transform.forward * speed;
 
         //get player inputs 
         Vector3 yaw = Input.GetAxis("Horizontal") * transform.right * rotSpeedX * Time.deltaTime;
-
-        Vector3 pitch = Input.GetAxis("Vertical") * transform.up * rotSpeedY * Time.deltaTime;
-
+        
+        if(isAboutToCollide)
+        {
+            pitch = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            pitch = Input.GetAxis("Vertical") * transform.up * rotSpeedY * Time.deltaTime;
+        }
         //translate this inputs into plane movement
         Vector3 dir = yaw + pitch;
 
@@ -151,12 +169,23 @@ public class PlaneMovement : MonoBehaviour
         }
         else if (transform.eulerAngles.x <= 30.0f)
         {
-            //going down
-            return new AngleVelocity
+            if(!isAboutToCollide)
             {
-                currSpeed = Mathf.Lerp(currSpeed, baseSpeed, Time.deltaTime * 0.5f),
-                baseSpeed = Mathf.Lerp(baseSpeed, 30.0f, Time.deltaTime * 1.5f)
-            };
+                //going down
+                return new AngleVelocity
+                {
+                    currSpeed = Mathf.Lerp(currSpeed, baseSpeed, Time.deltaTime * 0.5f),
+                    baseSpeed = Mathf.Lerp(baseSpeed, 30.0f, Time.deltaTime * 1.5f)
+                };
+            }
+            else
+            {
+                return new AngleVelocity
+                {
+                    currSpeed = currSpeed,
+                    baseSpeed = baseSpeed
+                };
+            }
         }
         else if (transform.eulerAngles.x >= 320.0f)
         {
@@ -197,6 +226,68 @@ public class PlaneMovement : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+
+    private void GroundHelper()
+    {
+        if (isHelperOn)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, moveVector, out hit, 500.0f))
+            {
+                Debug.DrawRay(transform.position, moveVector * 500.0f);
+                if (hit.collider.tag == "floor")
+                {
+                    if(hit.distance <= 100)
+                    {
+                        isAboutToCollide = true;   
+                    }
+                }
+               
+            }
+            if (isAboutToCollide)
+            {
+                currSpeed = Mathf.Lerp(currSpeed, baseSpeed, Time.deltaTime * 5);
+                baseSpeed = Mathf.Lerp(baseSpeed, 10.0f, Time.deltaTime * 7);
+                transform.eulerAngles = new Vector3(Mathf.LerpAngle(transform.eulerAngles.x, -120, Time.deltaTime / 40f), transform.eulerAngles.y, transform.eulerAngles.z);
+
+                Debug.Log(transform.rotation.eulerAngles);
+
+                if (transform.rotation.eulerAngles.x < 5.0f)
+                {
+                    isAboutToCollide = false;
+                }
+            }
+        }
+    }
+    private void StallingHandler()
+    {
+        if (isHelperOn)
+        {
+            if (transform.eulerAngles.x <= 320 && transform.eulerAngles.x >315.0f && Input.GetKey(KeyCode.W))
+            {
+                rotSpeedY = 0.0f;
+            }
+            else
+            {
+                rotSpeedY = 3.0f;
+            }
+            if (currSpeed < 5.0f)
+            {
+                currSpeed = Mathf.Clamp(currSpeed, 5.0f, 10.0f);
+            }
+        }
+    }
+
+    private void TurnHelper()
+    {
+        if(isHelperOn)
+        {
+            if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            {       
+                currSpeed -= 0.01f;
+            }
         }
     }
 }
