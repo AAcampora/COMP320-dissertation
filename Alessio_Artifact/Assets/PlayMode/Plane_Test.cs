67be0927bb20b2191c4ce4ac39b34631c07bbd0c
Alessio Acampora <aa202405@falmouth.ac.uk>
2021-07-15 14:14:55 +0100

    cleaned unit tests

--- Alessio_Artifact/Assets/PlayMode/Plane_Test.cs
+++ Alessio_Artifact/Assets/PlayMode/Plane_Test.cs
@@ -12,7 +12,104 @@ namespace Tests
         // `yield return null;` to skip a frame.
         [UnityTest]
         //sut_when_then
-        public IEnumerator North_Check_Active()
+        public IEnumerator isPlaneSpawned_Test()
+        {
+            // Use the Assert class to test conditions.
+
+            //arrange
+            var gameObject = new GameObject();
+            var plane = gameObject.AddComponent<Plane>();
+
+            // Use yield to skip a frame.
+            //act
+            yield return new WaitForFixedUpdate();
+
+            //assert
+            Assert.AreEqual(10.0f, plane.North);
+        }
+        [UnityTest]
+        public IEnumerator isLandingGearOn_test()
+        {
+            // Use the Assert class to test conditions.
+
+            //arrange
+            var gameObject = new GameObject();
+            var plane = gameObject.AddComponent<Plane>();
+
+            // Use yield to skip a frame.
+            //act
+            yield return new WaitForFixedUpdate();
+
+            //assert
+            Assert.AreEqual(10.0f, plane.North);
+        }
+        [UnityTest]
+        public IEnumerator isHelperActive_test()
+        {
+            // Use the Assert class to test conditions.
+
+            //arrange
+            var gameObject = new GameObject();
+            var plane = gameObject.AddComponent<Plane>();
+
+            // Use yield to skip a frame.
+            //act
+            yield return new WaitForFixedUpdate();
+
+            //assert
+            Assert.AreEqual(10.0f, plane.North);
+        }
+
+        [UnityTest]
+        public IEnumerator isSpeedNormal_test()
+        {
+            // Use the Assert class to test conditions.
+
+            //arrange
+            var gameObject = new GameObject();
+            var plane = gameObject.AddComponent<Plane>();
+
+            // Use yield to skip a frame.
+            //act
+            yield return new WaitForFixedUpdate();
+
+            //assert
+            Assert.AreEqual(10.0f, plane.North);
+        }
+        [UnityTest]
+        public IEnumerator isHelperDiveWorking_test()
+        {
+            // Use the Assert class to test conditions.
+
+            //arrange
+            var gameObject = new GameObject();
+            var plane = gameObject.AddComponent<Plane>();
+
+            // Use yield to skip a frame.
+            //act
+            yield return new WaitForFixedUpdate();
+
+            //assert
+            Assert.AreEqual(10.0f, plane.North);
+        }
+        [UnityTest]
+        public IEnumerator isHelperTurnWorking_test()
+        {
+            // Use the Assert class to test conditions.
+
+            //arrange
+            var gameObject = new GameObject();
+            var plane = gameObject.AddComponent<Plane>();
+
+            // Use yield to skip a frame.
+            //act
+            yield return new WaitForFixedUpdate();
+
+            //assert
+            Assert.AreEqual(10.0f, plane.North);
+        }
+        [UnityTest]
+        public IEnumerator isHelperStallWorking_test()
         {
             // Use the Assert class to test conditions.
 
